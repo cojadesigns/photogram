f3c7973fe79e6038536a8184fd58763a125c0a78
Akitoye O. Joseph <cojadesigns@gmail.com>
2016-05-03 15:03:58 +0100

    Generated post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,23 @@ class PostsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
   def show
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:caption)
   end
 end
