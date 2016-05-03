69325a2936e6d4bee20e87d7287eae727eaec034
Akitoye O. Joseph <cojadesigns@gmail.com>
2016-05-03 15:46:33 +0100

    Changed template language from ERB to HAML

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
