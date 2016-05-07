dc92003350d42d1a5d397e87c8cb99ca688eb40e
cojadesigns <cojadesigns@gmail.com>
2016-05-07 01:32:36 +0100

    Refactored the index, show view and added proper styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,32 @@
-class CommentsController < ApplicationController
+class CommentsController < 
+	before_action :find_post
+	def create
+		@comment = @post.comments.build(comment_params)
+		@comment.user_id = current_user.id
+
+		if @comment.save
+			flash[:success] = "You commented the hell out of that post"
+			redirect_to :back
+		else
+			flash[:error] = "Check the comment form. Something went horribly wrong"
+			render root_path
+		end
+	end
+
+	def destroy
+		@comment = @post.comments.find(params[:id])
+
+		@comment.destroy
+		flash[:success] = "Comment was successfully deleted :("
+		redirect_to root_path
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
 end
