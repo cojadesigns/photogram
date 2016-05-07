e09a9bea3c8779d5aee1dd47c25be40da511da56
cojadesigns <cojadesigns@gmail.com>
2016-05-07 01:03:57 +0100

    Generated comments model & configured associations between users, posts & comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates :image, presence: true
   validates :user_id, presence: true
+  has_many :comments
 
   has_attached_file :image, styles: { :medium => '640x'}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
